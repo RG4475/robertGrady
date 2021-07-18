a09380f0518b0b3eb33b074fb95b9abaf209111a
Robert <rgrady771@hotmail.com>
2021-07-18 02:01:47 +0100

    Finalised all APIs

--- task/js/script.js
+++ task/js/script.js
@@ -14,10 +14,12 @@ $('#buttonAPI1').click(function() {
             {
                 $('#field1').html("Airport ICAO Code: " + result['data']['ICAO']);
                 $('#field2').html("Station Name: " + result['data']['stationName']);
-                $('#field3').html("Temperature: " + result['data']['temperature']);
-                $('#field4').html("Humidity: " + result['data']['humidity']);
-                $('#field5').html("Clouds: " + result['data']['clouds']);
-                $('#field6').html("Wind Speed: " + result['data']['windSpeed']);
+                $('#field3').html("Country Code: " + result['data']['countryCode']);
+                $('#field4').html("Temperature: " + result['data']['temperature']);
+                $('#field5').html("Humidity: " + result['data']['humidity']);
+                $('#field6').html("Clouds: " + result['data']['clouds']);
+                $('#field7').html("Wind Speed: " + result['data']['windSpeed']);
+                $('#field8').html("Date/time of observation: " + result['data']['datetime']);
             }
         },
 
@@ -44,17 +46,55 @@ $('#buttonAPI2').click(function() {
 
             if(result.status.name == "OK")
             {
-                $('#field1').html("Datetime: " + result['data'][0]['datetime']);
-                $('#field2').html("Depth: " + result['data'][0]['depth']);
-                $('#field3').html("Lng: " + result['data'][0]['lng']);
-                $('#field4').html("Magnitude: " + result['data'][0]['magnitude']);
-                $('#field5').html("");
-                $('#field6').html("");
+
+                for(let i = 0; i < result['data'].length; i++)
+                {
+                    let rowNum = i + 2;
+                    $('tr.EarthquakeInfo:nth-child('+ rowNum +') td:nth-child(1)').html(result['data'][i]['datetime']);
+                    $('tr.EarthquakeInfo:nth-child('+ rowNum +') td:nth-child(2)').html(result['data'][i]['depth']);
+                    $('tr.EarthquakeInfo:nth-child('+ rowNum +') td:nth-child(3)').html(result['data'][i]['lng']);
+                    $('tr.EarthquakeInfo:nth-child('+ rowNum +') td:nth-child(4)').html(result['data'][i]['magnitude']);
+                    $('tr.EarthquakeInfo:nth-child('+ rowNum +') td:nth-child(5)').html(result['data'][i]['lat']);
+                    $('tr.EarthquakeInfo:nth-child('+ rowNum +') td:nth-child(6)').html("");
+                }
+
             }
         },
 
         error: function(jqXHR, textStatus, errorThrown) {
 
+        }
+    });
+});
+
+$('#buttonAPI3').click(function() {
+    $.ajax({
+        url: "php/getOcean.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#enterLatitude').val(),
+            lng: $('#enterLongitude').val()
+        },
+        success: function(result){
+
+            console.log(JSON.stringify(result));
+
+            if(result.status.name == "OK")
+            {
+                $('#field1').html("Ocean Name: " + result['data']['name']);
+                $('#field2').html("");
+                $('#field3').html("");
+                $('#field4').html("");
+                $('#field5').html("");
+                $('#field6').html("");
+                $('#field7').html("");
+                $('#field8').html("");
+            }
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            
         }
     });
 });
