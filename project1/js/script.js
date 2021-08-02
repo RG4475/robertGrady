9d349cb2ae20288b6bf622872fcc7b5195b71510
Robert <rgrady771@hotmail.com>
2021-08-03 00:44:32 +0100

    Put in the code determining the user's current location using the Javascript navigator geolocation command

--- project1/js/script.js
+++ project1/js/script.js
@@ -4,64 +4,31 @@ $(window).on('load', function() {
             $(this).remove();
         });
     }
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-
-    var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-    var circle = L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(mymap);
-
-    var polygon = L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ]).addTo(mymap);
-
-    marker.bindPopup("Hello this is a marker").openPopup();
-    circle.bindPopup("Hello this is a circle");
-    polygon.bindPopup("Hello this is a polygon");
-
-    var popup1 = L.popup()
-        .setLatLng([51.5, -0.09])
-        .setContent("This is a standalone popup.")
-        .openOn(mymap);
-
-    function onMapClick1(e) {
-        alert("You clicked the map at these coordinates" + e.latlng);
-    }
-
-    //mymap.on('click', onMapClick1);
-
-    var popup2 = L.popup();
-
-    function onMapClick2(e) {
-        popup2
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at these coordinates " + e.latlng.toString())
-            .openOn(mymap);
-    }
-
-    mymap.on('click', onMapClick2);
-});
-
-$('#locationInfo').click(function() {
+    var mymap = L.map('mapid');
+    var currentPosMark;
 
     function showPositionInfo(position) {
-        $('#locationInfo').html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+        mymap.setView([position.coords.latitude, position.coords.longitude], 13);
+        
     }
+
+    function showPositionMarker(position) {
+        currentPosMark = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap);
+    }
+
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPositionInfo);
+
+        var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+
+        navigator.geolocation.getCurrentPosition(showPositionMarker);
     }
+
     else {
-        $('#locationInfo').html("Geolocation not supported");
+        $('#currentLocMessage').html("Unfortunately your web browser does not support Geolocation");
     }
-})
+
+});
