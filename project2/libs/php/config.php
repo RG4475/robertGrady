b42ad1d0f30f99180c2433c1e2d3cd60e08650cc
Robert <rgrady771@hotmail.com>
2021-09-14 16:11:50 +0100

    Started making project2

---
+++ project2/libs/php/config.php
@@ -0,0 +1,16 @@
+
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "u802299405_company_direct";
+	$cd_user = "u802299405_RobertDatas";
+	$cd_password = "Database1";
+
+?>
