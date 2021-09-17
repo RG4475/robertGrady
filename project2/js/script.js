2ed790e65d28b9bf3e0d96af687245d373c1bd55
Robert <rgrady771@hotmail.com>
2021-09-17 08:00:17 +0100

    Created Add and Modify modals for all tables, php files to pick up all the data from the tables and ajax requests to pick up the data from the php files though this does not populate the website yet apart from the Department table as still awaiting preferred method on populating the website with the data

--- project2/js/script.js
+++ project2/js/script.js
@@ -23,6 +23,24 @@ $(window).on('load', function() {
         focus: true
     });
 
+    var modifyLocationModal = new bootstrap.Modal(document.getElementById('modifyLocationModal'), {
+        backdrop: true,
+        keyboard: true,
+        focus: true
+    });
+
+    var modifyDepartmentModal = new bootstrap.Modal(document.getElementById('modifyDepartmentModal'), {
+        backdrop: true,
+        keyboard: true,
+        focus: true
+    });
+
+    var modifyPersonnelModal = new bootstrap.Modal(document.getElementById('modifyPersonnelModal'), {
+        backdrop: true,
+        keyboard: true,
+        focus: true
+    });
+
     $('#addLocation').click(function() {
         addLocationModal.show();
     });
@@ -34,6 +52,19 @@ $(window).on('load', function() {
     $('#addPersonnel').click(function() {
         addPersonnelModal.show();
     });
+
+    $('#modifyLocation').click(function() {
+        modifyLocationModal.show();
+    });
+
+    $('#modifyDepartment').click(function() {
+        modifyDepartmentModal.show();
+    });
+
+    $('#modifyPersonnel').click(function() {
+        modifyPersonnelModal.show();
+    });
+
     $.ajax({
         url: "libs/php/getAllDepartments.php",
         type: 'GET',
@@ -44,25 +75,31 @@ $(window).on('load', function() {
 
             if(result.status.name == "ok")
             {
-                $('#errorMessage').html(result['data'].length);
+                //$('#errorMessage').html(result['data'].length);
                 
                 for(let i = 0; i < result['data'].length; i++)
                 {
                 var newDepartmentRow = document.createElement("tr");
-                var newDepartmentID = document.createElement("th");
+                var insertDepartmentID = document.createElement("th");
                 var IDtext = document.createTextNode(result['data'][i]['id']);
-                newDepartmentID.appendChild(IDtext);
-                newDepartmentRow.appendChild(newDepartmentID);
+                insertDepartmentID.appendChild(IDtext);
+                newDepartmentRow.appendChild(insertDepartmentID);
 
-                var newDepartmentName = document.createElement("td");
+                var insertDepartmentName = document.createElement("td");
                 var departmentNameText = document.createTextNode(result['data'][i]['name']);
-                newDepartmentName.appendChild(departmentNameText);
-                newDepartmentRow.appendChild(newDepartmentName);
+                insertDepartmentName.appendChild(departmentNameText);
+                newDepartmentRow.appendChild(insertDepartmentName);
 
-                var newDepartmentlocationID = document.createElement("td");
+                var insertDepartmentlocationID = document.createElement("td");
                 var locationIDText = document.createTextNode(result['data'][i]['locationID']);
-                newDepartmentlocationID.appendChild(locationIDText);
-                newDepartmentRow.appendChild(newDepartmentlocationID);
+                insertDepartmentlocationID.appendChild(locationIDText);
+                newDepartmentRow.appendChild(insertDepartmentlocationID);
+
+                /*
+                var hiddenDepartmentID = document.createElement("input");
+                var hiddenDeptIDText = document.createAttribute("type");
+                */
+
 
                 var departmentTable = document.getElementById("departmentTable");
                 departmentTable.getElementsByTagName("tbody")[0].appendChild(newDepartmentRow);
@@ -79,7 +116,49 @@ $(window).on('load', function() {
 
                 
             }
+        },
+
+        error: function(jqXHR, textStatus, errorThrown){
+
         }
 
+    });
+
+    $.ajax({
+        url: "libs/php/getAllPersonnel.php",
+        type: 'GET',
+        dataType: 'json',
+
+        success: function(result, status, xhr) {
+            console.log(JSON.stringify(result));
+
+            if(result.status.name == "ok")
+            {
+                //$('#errorMessage').html(result['data'].length);
+            }
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+
+        }
+    });
+
+    $.ajax({
+        url: "libs/php/getAllLocations.php",
+        type: 'GET',
+        dataType: 'json',
+
+        success: function(result, status, xhr) {
+            console.log(JSON.stringify(result));
+
+            if(result.status.name == "ok")
+            {
+                $('#errorMessage').html(result['data'].length);
+            }
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+
+        }
     })
 });
