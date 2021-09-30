f71bcb6fbdfa7f29ba45bcffd7835e4740e6f2f2
Robert <rgrady771@hotmail.com>
2021-09-30 09:30:57 +0100

    Attempted conversion of table names to English programmatically

---
+++ project2/js/Conversion of table names to English.js
@@ -0,0 +1,13 @@
+    /*
+    let str = "firstName";
+    let myArr = str.match(/[A-Z]/g);
+    let replacingStrings = str.replace(myArr, ","+myArr);
+    let splitStrings = replacingStrings.split(",");
+
+    let firstLetter = splitStrings[0].charAt(0).toUpperCase();
+    let capitaliseFirstWord = firstLetter + splitStrings[0].substr(1)
+    let concatStrings = capitaliseFirstWord.concat(" " + splitStrings[1]);
+
+    document.getElementById("demo").innerHTML = concatStrings;
+
+    */
--- project2/js/script.js
+++ project2/js/script.js
@@ -35,20 +35,13 @@ function generateChosenTable(table, data) {
     let myArr = str.match(/[A-Z]/g);
     let replacingStrings = str.replace(myArr, ","+myArr);
     let splitStrings = replacingStrings.split(",");
-    document.getElementById("demo").innerHTML = splitStrings;
-    */
 
-    /*
-    let str = "firstName";
-    let myArr = str.match(/[A-Z]/g);
-    let splitString = str.split(/(myArr)/);
-    document.getElementById("demo").innerHTML = splitString;
-    */
+    let firstLetter = splitStrings[0].charAt(0).toUpperCase();
+    let capitaliseFirstWord = firstLetter + splitStrings[0].substr(1)
+    let concatStrings = capitaliseFirstWord.concat(" " + splitStrings[1]);
 
-    /*
-    let string = "firstName";    
-    let splitString = string.split(/([A-Z])/);
-    document.getElementById("demo").innerHTML = splitString;
+    document.getElementById("demo").innerHTML = concatStrings;
+
     */
 }
 
